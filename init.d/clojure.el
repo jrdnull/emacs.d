6ed86a83d812dee9059b539cf27d816215c6ade3
Jordon Smith <jordon@jordonsmith.uk>
2015-11-17 15:36:22 +0000

    Smartparens needs setting up still, multiple cursors

---
+++ init.d/clojure.el
@@ -0,0 +1,7 @@
+(use-package clojure-mode
+  :config
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+  (subword-mode +1)
+)
+
+(use-package cider)
---
+++ init.d/coffee.el
@@ -0,0 +1,4 @@
+(use-package coffee-mode
+  :config
+  (add-to-list 'coffee-args-compile "--no-header")
+  (subword-mode +1))
